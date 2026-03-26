fb1dec60b68fabbf355720a5172c1a67fa73a4de
erickramossantoser <erickramos.santoser@gmail.com>
2026-03-26 09:25:48 -0300

    arrumando o codigo novamente

--- src/pages/Integrantes.tsx
+++ src/pages/Integrantes.tsx
@@ -2,45 +2,8 @@ import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Layout from '../components/Layout'
 
-export interface Integrante {
-  id: string
-  nome: string
-  rm: string
-  turma: string
-  github: string
-  linkedin: string
-  emoji: string
-}
-
-export const integrantes: Integrante[] = [
-  {
-    id: '1',
-    nome: 'Erick Ramos',
-    rm: '567837',
-    turma: '1TDSPB',
-    github: 'https://github.com/erickramossantoser-create',
-    linkedin: '#',
-    emoji: '👤',
-  },
-  {
-    id: '2',
-    nome: 'Guilherme de Sousa Leopoldino',
-    rm: '567242',
-    turma: '1TDSPB',
-    github: 'https://github.com/GuiDSLeopoldino',
-    linkedin: '#',
-    emoji: '👤',
-  },
-  {
-    id: '3',
-    nome: 'Lucas Cunha Maia Vila Nova',
-    rm: '566742',
-    turma: '1TDSPB',
-    github: 'https://github.com/lucascunhamaia',
-    linkedin: '#',
-    emoji: '👤',
-  },
-]
+// IMPORTANTE: Importamos os dados e a interface do arquivo que você criou
+import { integrantes } from '../data/integrantes' 
 
 const Integrantes = () => {
   useEffect(() => {
@@ -63,6 +26,7 @@ const Integrantes = () => {
             <p className="text-slate-400">Equipe multidisciplinar unida pelo propósito de transformar vidas através da tecnologia</p>
             <div className="w-20 h-1 bg-blue-500 mx-auto mt-4" />
           </div>
+          
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             {integrantes.map((integrante) => (
               <Link
