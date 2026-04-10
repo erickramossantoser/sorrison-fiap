40ffc6c1ffa4a6ccc8423426561b83894b2753c5
saboy71 <samuelgaleati@gmail.com>
2026-04-10 10:21:54 -0300

    colocando a imagem nos integrantes e arrumando informações

--- src/data/integrantes.ts
+++ src/data/integrantes.ts
@@ -6,10 +6,11 @@ export interface Integrante {
   github: string
   linkedin: string
   emoji: string
+  foto?: string // Caminho da imagem do integrante
 }
 
 export const integrantes: Integrante[] = [
-  { id: '1', nome: 'Erick Ramos', rm: '567837', turma: '1TDSPB', github: 'https://github.com/erickramossantoser', linkedin: 'https://www.linkedin.com/in/erickrsantos/', emoji: '👤' },
-  { id: '2', nome: 'Guilherme Marques', rm: '568054', turma: '1TDSPA', github: 'github.com/GuiDSLeopoldino', linkedin: 'www.linkedin.com/in/guilherme-marques-15670b307', emoji: '👤' },
-  { id: '3', nome: 'Lucas Cunha Maia Vila Nova', rm: '566742', turma: '1TDSPB', github: 'https://github.com/lucascunhamaia', linkedin: '#', emoji: '👤' },
+  { id: '1', nome: 'Erick Ramos', rm: '567837', turma: '1TDSPB', github: 'https://github.com/erickramossantoser', linkedin: 'https://www.linkedin.com/in/erickrsantos/', emoji: '👤', foto: '/src/img/erick.jpeg' },
+  { id: '2', nome: 'Guilherme Marques', rm: '568054', turma: '1TDSPA', github: 'github.com/GuiDSLeopoldino', linkedin: 'www.linkedin.com/in/guilherme-marques-15670b307', emoji: '👤', foto: '/src/img/guilherme.jpeg' },
+  { id: '3', nome: 'Samuel Galeati', rm: '566742', turma: '1TDSPB', github: 'https://github.com/saboy71', linkedin: '#', emoji: '👤', foto: '/src/img/samuel.jpeg' },
 ]
